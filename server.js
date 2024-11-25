1a0ee4130eb12050f1dc4b7c24cd70ac055960c5
Gabriel <gabrielffloriano1@gmail.com>
2024-11-25 20:38:47 -0300

    Aula 5 - Deploy

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import routes from "./src/routes/postsRoutes.js";
 
 const app = express(); // Cria uma instância do servidor Express
-routes(app)
+app.use(express.static("uploads"));
+routes(app);
 
 // Inicia o servidor na porta 3000 e exibe uma mensagem no console quando o servidor estiver ouvindo
 app.listen(3000, () => {
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,26 +1,44 @@
 import express from "express";
 import multer from "multer";
-import {listarPosts, postarNovoPost, uploadImagem} from "../controllers/postsController.js";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
+import cors from "cors";
 
-// config pra fazer o multer funcionar no windows
+const corsOptions = {
+  origin:"http://localhost:8000",
+  optionsSuccessStatus: 200
+}
+
+// Configuração para armazenamento de imagens com multer (específico para Windows)
 const storage = multer.diskStorage({
-  destination: function (req, file, cb){
+  destination: function (req, file, cb) {
+    // Define o diretório para salvar as imagens enviadas: 'uploads/'
     cb(null, 'uploads/');
   },
-  filename: function(req,file, cb){
+  filename: function (req, file, cb) {
+    // Mantém o nome original do arquivo enviado
     cb(null, file.originalname);
   }
-})
+});
 
-const upload = multer({dest:"./uploads", storage});
+// Cria uma instância do middleware multer utilizando a configuração 'storage'
+const upload = multer({ dest: "./uploads", storage });
 
+// Função para definir as rotas da aplicação
 const routes = (app) => {
-    // Habilita a interpretação de requisições com formato JSON
-    app.use(express.json());
-    // Rota para buscar todos os posts
-    app.get("/posts", listarPosts);
-    app.post("/posts", postarNovoPost);
-    app.post("/upload", upload.single("imagem"), uploadImagem); // form-data: Key=imagem
-}
+  // Habilita o parseamento de requisições no formato JSON
+  app.use(express.json());
+  app.use(cors(corsOptions))
+  // Rota GET para listar todos os posts (implementada em postsController.js)
+  app.get("/posts", listarPosts);
+
+  // Rota POST para criar um novo post (implementada em postsController.js)
+  app.post("/posts", postarNovoPost);
+
+  // Rota POST para upload de imagem (usa o middleware 'upload.single("imagem")')
+  // O parâmetro 'imagem' define o campo do formulário que conterá a imagem
+  app.post("/upload", upload.single("imagem"), uploadImagem); // form-data: Key=imagem
+
+  app.put("/upload/:id", atualizarNovoPost);
+};
 
 export default routes;
